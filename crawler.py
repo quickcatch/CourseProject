e445aeec665c7a17ac2c63c79bb561ca44092b49
trevorm4 <trevorm4@illinois.edu>
2021-11-13 23:27:24 -0600

    Implemented crawler with no filtering

--- crawler.py
+++ crawler.py
@@ -2,6 +2,8 @@ from bs4 import BeautifulSoup as bs
 import requests
 from bs4.element import Comment
 import datetime
+from os import path,mkdir
+import validators
 
 def should_check_url(url : str, blacklisted_sites : dict):
     split = url.split(".")
@@ -16,10 +18,15 @@ def get_blacklisted_sites(file_name):
         return f.readlines()
 
 def get_html(url):
-    r = requests.get(url)
-    if r.status_code < 200 or r.status_code > 299: # error ocurred
-        return None 
-    return r.text
+    try:
+        r = requests.get(url, timeout=10)
+        if r.status_code < 200 or r.status_code > 299: # error ocurred
+            print(f"Error getting html for {url} with status code {r.status_code}")
+            return None 
+        return r.text
+    except Exception as e:
+        print(f"Error getting html for {url} due to timeout")
+
 
 def tag_visible(element):
     if element.parent.name in ['a','style', 'script', 'head', 'title', 'meta', '[document]']:
@@ -28,9 +35,12 @@ def tag_visible(element):
         return False
     return True
 
+def is_valid_url(url):
+    return validators.url(url)
+
 def get_all_articles(hn_url):
     soup = bs(get_html(hn_url),'html.parser')
-    return [a['href'] for a in soup.find_all('a',{'class':'titlelink'})]
+    return [a['href'] for a in soup.find_all('a',{'class':'titlelink'}) if "ycombinator" not in a['href'] and is_valid_url(a["href"])]
 
 
 def text_from_html(body):
@@ -46,11 +56,51 @@ def all_dates(year, month, date):
         end_date = datetime.date.today()
     print(end_date)
     delta = datetime.timedelta(days=1)
-
+ 
     all_days = list()
 
     while start_date <= end_date:
-    	date = start_date.strftime("%m%d%Y")
-    	all_days.append(date)
-    	start_date += delta
+        date = start_date.strftime("%Y-%m-%d")
+        all_days.append(date)
+        start_date += delta
     return all_days
+
+def get_articles_for_date(date):
+    page_num = 1
+    current_url = f"https://news.ycombinator.com/front?day={date}&p={page_num}"
+    current_articles = get_all_articles(current_url)
+    articles = []
+    while len(current_articles) > 0:
+        for a in current_articles:
+            articles.append(a)
+        page_num += 1
+        current_url = f"https://news.ycombinator.com/front?day={date}&p={page_num}"
+        current_articles = get_all_articles(current_url)
+    return articles
+
+def url_to_filename(url):
+    return url.replace("/", "{").replace(":","}")
+
+def crawl(start_year, start_month, start_day):
+    dates = all_dates(start_year, start_month, start_day)
+    if not path.isdir('data'):
+        mkdir('data')
+    for d in dates:
+        articles = get_articles_for_date(d)
+        dir_name = path.join('data',d)
+        print(dir_name)
+        if not path.isdir(dir_name):
+            print(f"creating {dir_name}")
+            mkdir(dir_name)
+        for a in articles:
+            print(a)
+            body = get_html(a)
+            if body == None:
+                continue
+            text = text_from_html(body)
+            file_name = path.join(dir_name,url_to_filename(a))
+            with open(file_name,'w+') as f:
+                f.write(text)
+
+if __name__ == "__main__":
+    crawl(2021,11,2)
