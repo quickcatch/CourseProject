fbe6fd4553ed241a38a0bbb1954d452ea4f5113c
trevorm4 <trevorm4@illinois.edu>
2021-12-08 10:52:59 -0600

    api works

--- api.py
+++ api.py
@@ -1,23 +1,28 @@
 from sklearn.cluster import KMeans
 from fastapi import FastAPI
-from clustering import classify
+from clustering import classify, CosSimilarity, get_similar_docs
 from joblib import load
 from sklearn.feature_extraction.text import TfidfVectorizer
 import os
 app = FastAPI()
 cluster_dir = "clusters"
+data_dir = "data_entire"
 url_df = None
 model = None
 vectorizer = None
+cos = None
 
 @app.on_event("startup")
 async def startup():
-    model = load(os.path.join(cluster_dir,"kmeans.pkl"))
-    url_df = load(os.path.join(cluster_dir,"URL-cluster.pkl"))
-    vectorizer = load(os.path.join(cluster_dir,"vectorizer"))
+    global model, url_df, cos, vectorizer
+    model = load(os.path.join(cluster_dir,"model.pkl"))
+    url_df = load(os.path.join(cluster_dir,"dataframe.pkl"))
+    vectorizer = load(os.path.join(cluster_dir,"vectorizer.pkl"))
+    cos = CosSimilarity(data_dir,model, url_df, vectorizer)
 
-
-@app.get("/classify/{url}")
+@app.get("/classify/{url:path}")
 async def read_item(url):
-    results = classify(url,cluster_dir)
-    return {"results": results}
+    global cos
+    print(cos,vectorizer,url_df,model)
+    results = get_similar_docs(url,cos)
+    return {"docs": results}
